f5ba7b1f2410c8af5d52c54e924551347f9d30c2
straysh <jobhancao@gmail.com>
2017-10-29 20:02:50 +0800

    遗留Function.__proto__和Function.prototype问题,鸡蛋悖论

--- CH1/prototype.js
+++ CH1/prototype.js
@@ -26,6 +26,11 @@ describe("对象的_proto_和prototype属性", function(){
     assert.strictEqual(Person, Person.prototype.constructor);
   });
 
+  it("实例的__proto__.__proto__即Object的prototype", function(){
+    var person = new Person();
+    assert.strictEqual(person.__proto__.__proto__, Object.prototype);
+  });
+
   /**
    * 当获取 person.constructor 时，其实 person 中并没有 constructor 属性,当不能读取到constructor 属性时，
    * 会从 person 的原型也就是 Person.prototype 中读取，正好原型中有该属性，所以：
@@ -33,11 +38,29 @@ describe("对象的_proto_和prototype属性", function(){
   it("实例的构造函数和构造函数的原型的构造函数", function(){
     var person = new Person();
     assert(person.constructor === Person.prototype.constructor);
+    assert(person.hasOwnProperty('constructor') === false);
+    assert(Person.prototype.hasOwnProperty('constructor') === true);
+  });
+
+  /**
+   * person中并没有__proto__属性, person.__proto__实际上调用了Object.getPrototypeOf(person)
+   * Person.prototype.__proto__ 实际上调用了 Object.getPrototypeOf(Person.prototype)
+   */
+  it("__prot__是由Object.getPrototypeOf( instance )返回的", function(){
+    var person = new Person();
+    assert( person.hasOwnProperty('__proto__') === false );
+    assert( person.__proto__ === Object.getPrototypeOf(person) );
   });
 
   it("一些额外的测试", function(){
     var obj = new Object();
-    assert(obj.__proto__, Object.prototype);
-    assert(Person.prototype.__proto__, Object.prototype);
+    assert(obj.__proto__ === Object.prototype);
+    assert(Person.prototype.__proto__ === Object.prototype);
+  });
+
+  it("Function.__proto__ === Function.prototype", function(){
+    assert( Function.__proto__ === Function.prototype );
+    assert( Function.hasOwnProperty('__proto__') === false );
+    console.log( Function.__proto__, Function.prototype, Object.getPrototypeOf(Function) );
   });
 });
